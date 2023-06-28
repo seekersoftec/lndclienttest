47adf22aaeed5948d6aefe65e604218136626fd1
aruokhai <joshuaaruokhaitech@gmail.com>
2023-06-28 13:01:50 +0100

    initial commit

---
+++ lndpay/lndpay.go
@@ -0,0 +1,69 @@
+package lndpay
+
+import (
+	"context"
+
+	"github.com/lightninglabs/lndclient"
+	"github.com/lightningnetwork/lnd/lnrpc"
+)
+
+type Service struct {
+	context     context.Context
+	lnrpcClient lnrpc.LightningClient
+}
+
+func NewInstance(lndHost string, tlsPath string, macarroonDir string, networkType string) (*Service, error) {
+	lnrpcClient, err := lndclient.NewBasicClient(lndHost, tlsPath, macarroonDir, networkType, lndclient.MacFilename("lnd_admin.macaroon"))
+	if err != nil {
+		return nil, err
+	}
+
+	service := &Service{
+		context:     context.Background(),
+		lnrpcClient: lnrpcClient,
+	}
+	return service, nil
+}
+
+// Get Infomation
+func (s *Service) GetInfo() (*lnrpc.GetInfoResponse, error) {
+	infoRequest := &lnrpc.GetInfoRequest{}
+	return s.lnrpcClient.GetInfo(s.context, infoRequest)
+}
+
+func (s *Service) GetNetworkInfo() (*lnrpc.NetworkInfo, error) {
+	infoRequest := &lnrpc.NetworkInfoRequest{}
+	return s.lnrpcClient.GetNetworkInfo(s.context, infoRequest)
+}
+
+func (s *Service) GetTransactions() (*lnrpc.TransactionDetails, error) {
+	infoRequest := &lnrpc.GetTransactionsRequest{}
+	return s.lnrpcClient.GetTransactions(s.context, infoRequest)
+}
+
+func (s *Service) GetChannelBalance() (*lnrpc.ChannelBalanceResponse, error) {
+	infoRequest := &lnrpc.ChannelBalanceRequest{}
+	return s.lnrpcClient.ChannelBalance(s.context, infoRequest)
+}
+
+// List channels, payments made and Invoices
+func (s *Service) ListChannels() (*lnrpc.ListChannelsResponse, error) {
+	infoRequest := &lnrpc.ListChannelsRequest{}
+	return s.lnrpcClient.ListChannels(s.context, infoRequest)
+}
+
+func (s *Service) ListPayments() (*lnrpc.ListPaymentsResponse, error) {
+	infoRequest := &lnrpc.ListPaymentsRequest{}
+	return s.lnrpcClient.ListPayments(s.context, infoRequest)
+}
+
+func (s *Service) ListInvoices() (*lnrpc.ListInvoiceResponse, error) {
+	infoRequest := &lnrpc.ListInvoiceRequest{}
+	return s.lnrpcClient.ListInvoices(s.context, infoRequest)
+}
+
+// Add Invoice
+func (s *Service) AddInvoice() (*lnrpc.AddInvoiceResponse, error) {
+	infoRequest := &lnrpc.Invoice{}
+	return s.lnrpcClient.AddInvoice(s.context, infoRequest)
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Bitspend01/lndclienttest/lndpay"
+)
+
+func main() {
+	lndHost := "34.28.120.31:10009"
+	tlsPath := "lnd_tls.cert"
+	macDir := "./nam"
+	network := "testnet" // testnet
+
+	lndpayService, err := lndpay.NewInstance(lndHost, tlsPath, macDir, network)
+	if err != nil {
+		panic(err)
+	}
+
+	info, err := lndpayService.GetInfo()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(&info)
+}
